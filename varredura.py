3d5eb2c99687cef5b33e3432245688f2df8cff76
Luisa Toledo <luisavtoledo@gmail.com>
2022-10-30 23:33:08 -0300

    Correções

--- varredura.py
+++ varredura.py
@@ -3,7 +3,7 @@ from envoltoria import *
 #Função "sobre_segmento" verifica se um ponto está sobre um segmento formado por outros dois pontos
 #Entrada: lista de pontos [x, y]
 def sobre_segmento(p1, p2, p3):
-    #Verifica se o ponto p3 está dentro do intervalo do seguimento definido por p1 e p2
+    #Verifica se o ponto p3 está dentro do intervalo do segmento definido por p1 e p2
     if((p1[0] <= p3[0]) and (p3[0] <= p2[0])) and (((p1[1] <= p3[1]) and (p3[1] <= p2[1])) or ((p1[1] >= p3[1]) and (p3[1] >= p2[1]))):
         return True
     else:
@@ -34,6 +34,7 @@ def interceptam(p1, p2, p3, p4):
     else:
         return False
 
-#Função "segmentos_interceptam" usa a função "interceptam" para ver se dois segmentos vizinhos da árvos T se interceptam
+#Função "segmentos_interceptam" usa a função "interceptam" para ver se dois segmentos vizinhos da árvore T se interceptam
+def segmentos_interceptam()
 
 #Função "varredura_linear" usa a função "segmentos_interceptam" para ver se há interseção em um conjunto de segmentos
